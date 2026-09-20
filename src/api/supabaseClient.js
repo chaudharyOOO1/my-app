56a549a5196cbff2dd11f94ac93d7f922932fc18
chaudharyOOO1 <67969881+chaudharyOOO1@users.noreply.github.com>
2026-09-20 16:18:45 +0530

    Update supabaseClient.js

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zvogktuqdcpjsfargewg.supabase.co";
-const supabaseKey = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+const supabaseKey = "sb_publishable_nxCLI2Lj9Io__yHL2gxtTg_ybbfGWD4";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
